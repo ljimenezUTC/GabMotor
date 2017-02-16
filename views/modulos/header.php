ed900b52310b610bec73b29370bd33005e8399ae
Vinicio <vinichasiluisa@gmail.com>
2017-02-16 16:50:04 -0500

    Comentario Boton Menu
    header.php

--- views/modulos/header.php
+++ views/modulos/header.php
@@ -6,6 +6,8 @@
 	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
+
+	<!--Boton-->
 	<a class="navbar-brand" href="index.php?action=inicio">LOGO</a>
 
 

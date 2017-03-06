b5e5b37334a64331388915a15b61593eada27fcc
Mondragon <luisjimenez.r8@gmail.com>
2017-03-06 11:43:56 -0500

    Validacion sesiones

--- models/gestorMantenimientos.php
+++ models/gestorMantenimientos.php
@@ -1,7 +1,5 @@
 <?php 
 	
-	session_start();
-
 	require_once "conexion.php";
 	
 	class GestorMantenimientosModel
@@ -54,6 +52,8 @@
 		#AGREGAR LOS MANTENIMIENTOS EN LA TABLA TEMPORAL Y SU PRESENTACION EN LA VISTA AGREGAR MANTENIMIENTOS
 		#---------------------------------------------------------------------------------------------------
 		public function agregarMantenimientosModel($datosModel,$tablaTemporalMantenimientos, $tablaMantenimiento, $tablaCategoria){
+
+			session_start();
 
 			$sessionId = session_id();
 
@@ -114,7 +114,10 @@
 		#-----------------------------------------------------------------------------------------------
 		public function registrarMantenimientosModel($datosModel){
 
+			//session_start();
+
 			$usuario = $_SESSION["id_usuario"];
+
 
 			$stmt = Conexion::conectar()->prepare("INSERT INTO orden_pago (id_vehiculo, id_usuario, fecha_ingreso) VALUES(:idVehiculo, $usuario, NOW())");
 
